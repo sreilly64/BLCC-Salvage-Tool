36888913a9b9b2cd693a661aa8eceb289148cb18
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-09-01 11:34:13 -0400

    initial commit with spring boot project

---
+++ gw2-salvage-tool/src/test/java/sreilly64/com/github/gw2salvagetool/Gw2SalvageToolApplicationTests.java
@@ -0,0 +1,13 @@
+package sreilly64.com.github.gw2salvagetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gw2SalvageToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
